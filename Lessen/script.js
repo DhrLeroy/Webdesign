cd7bff251b01af3d3cabc6720e6c049fa0c16bb4
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-18 09:25:11 +0200

    first commit

---
+++ Lessen/script.js
@@ -0,0 +1,12 @@
+
+function test(e){
+    let p1 = document.getElementById("p1");
+    if(p1.getAttribute("data-waarde").valueOf() == "A")
+        p1.innerText = "A";
+    else
+        p1.innerText = "B";
+}
+
+function a(){
+
+}
